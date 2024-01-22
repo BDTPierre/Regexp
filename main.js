435018c2536ac5b1669df84adf34a8e3137df914
BDTPierre <buipierre@hotmail.fr>
2024-01-22 12:14:23 +0100

    Add telephone

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+//Rajouter le numéro de téléphone dans la fonction
+
+const number = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9];
+
+let username = document.getElementById("name").value;
+let userPhoneNumber = parseInt(document.getElementById("phone").value);
+
 // Étape 1 : le But de la Fonction
 // •	Objectif : Créer une fonction testAlpha qui vérifie si chaque caractère d'une chaîne de caractères (chaine) est présent dans un ensemble spécifié de caractères (alphabet).
 // •	Résultat attendu : La fonction doit renvoyer true si tous les caractères de chaine sont dans alphabet, sinon elle doit renvoyer false.
@@ -19,14 +26,17 @@ const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 // •	Si la boucle se termine sans trouver de caractère hors d’alphabet, cela signifie que tous les caractères sont valides. Donc, retournez true à la fin de la fonction.
 // Pensez à utiliser des console.log() pour debugger votre function et voir vos étapes, en appelant toujours la fonction une fois déclarer exemple.
 
-function testAlpha(chaine) {
-    for (let i = 0; i < chaine.length; i++) {
-        if (!alphabet.includes(chaine[i])) {
-            return false;
+function testAlpha (input , arrayCompare) {
+    for (let i = 0; i < input.length; i++) {
+        //On test si la valeur saisie par l'utilisateur est vraie ou fausse
+        if (arrayCompare.includes(input[i]) === false){
+            //Si la condition est remplie on renvoie false
+            return false
         }
     }
-    return true;
-}
+    //Si la condition du if n'est pas remplie on renvoie true
+    return true
+};
 
-console.log(testAlpha("Salut"))
-console.log(testAlpha("Salut23"))
+console.log(testAlpha(username , alphabet));
+console.log(testAlpha(userPhoneNumber , number));
